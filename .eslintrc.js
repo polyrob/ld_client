0a556de035b332d9a5bddc46e7524351efca7db8
rscheidt <robbiescheidt@gmail.com>
2018-03-10 09:57:42 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "node": true,
+        "jasmine": true
+    },
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "react/prefer-stateless-function": 0
+    }
+};
